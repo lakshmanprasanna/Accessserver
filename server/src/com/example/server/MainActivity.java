1ea6de73a996ad57f495acd1cea10997cf7aacf4
Lakshmanprasanna Venkitaramakrishnan <lakshmanprasanna@gmail.com>
2014-03-01 23:14:27 -0800

    Received on main activity

--- server/src/com/example/server/MainActivity.java
+++ server/src/com/example/server/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.app.Activity;
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 
-public class MainActivity extends FragmentActivity {
+
+public class MainActivity extends FragmentActivity implements AsyncResponse{
+	private static String TAG = "MainActivity";
+	
+	List<Data> m_dat = new ArrayList();
+	DownloadUsers us = new DownloadUsers();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		us.delegate = this;
+		 ConnectivityManager connMgr = (ConnectivityManager) 
+		            getSystemService(Context.CONNECTIVITY_SERVICE);
+		        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		        if (networkInfo != null && networkInfo.isConnected()) {
+		        	
+		        	us.execute("http://bismarck.sdsu.edu/photoserver/userlist");
+		            Log.i(TAG,"success");
+		        } else {
+		           Log.i(TAG,"Fail");
+		        }
 	}
 
 	@Override
@@ -19,5 +43,29 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public void processFinish(List<Data> output) {
+		ArrayList<String> str =new ArrayList();
+		Log.i(TAG,"Received");
+		m_dat = output;
+		for(int i=0;i<m_dat.size();i++)
+	    {
+	    	str.add(m_dat.get(i).user);
+	    }
+		for(int i=0;i<str.size();i++)
+	    {
+	    	System.out.println(str.get(i));
+	    }
+		
+		//Bundle b = new Bundle();
+	//	b.putStringArrayList("name", str);
+	//	MainFragment frag =new MainFragment();
+//		frag.setArguments(b);
+		//System.out.println(str.get(0));
+		
+	}
+	
+	 
 
 }
